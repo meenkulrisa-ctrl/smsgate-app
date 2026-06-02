44d929101fdba4a0cafbf7b7dad406b299614b71
meenkulrisa-ctrl <meen.kulrisa@gmail.com>
2026-06-03 03:34:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 // ==========================================
 // SMS Gateway - sms-gate.app
 // ==========================================
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $TOKEN     = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzbXMtZ2F0ZS5hcHAiLCJzdWIiOiJKVEZCTlAiLCJleHAiOjE3ODA0Mjc3NzIsImlhdCI6MTc4MDQyNjg3MiwianRpIjoibmJNYTFtQXh0V0xHNi1mU2RvY0twIiwidXNlcl9pZCI6IkpURkJOUCIsInNjb3BlcyI6WyJkZXZpY2VzOmxpc3QiLCJtZXNzYWdlczpyZWFkIiwibWVzc2FnZXM6d3JpdGUiLCJtZXNzYWdlczpzZW5kIiwibWVzc2FnZXM6bGlzdCJdfQ.j2r-eBtNf-BJTquS59rMGPhjHHuN2s0tm4UorJ0svYU";
 $DEVICE_ID = "U-ucDm6OQfO6FlCytxNIE";
 $API_BASE  = "https://sms-gate.app/3rdparty/v1";
@@ -48,6 +49,17 @@ function apiRequest($method, $endpoint, $body = null) {
 // Action: ส่ง SMS
 // ==========================================
 function sendSMS($phone, $message) {
+    const res = await fetch('', { method: 'POST', body: form });
+
+const text = await res.text();
+console.log(text);
+
+try {
+    const json = JSON.parse(text);
+    console.log(json);
+} catch(e) {
+    alert(text);
+}
     global $DEVICE_ID;
 
     // แปลงเบอร์เป็นรูปแบบ +66xxxxxxxxx

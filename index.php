a58ea93655203d35ca93c46eae398f06a5d26571
meenkulrisa-ctrl <meen.kulrisa@gmail.com>
2026-06-03 03:45:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // ==========================================
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$TOKEN     = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzbXMtZ2F0ZS5hcHAiLCJzdWIiOiJKVEZCTlAiLCJleHAiOjE3ODA0Mjc3NzIsImlhdCI6MTc4MDQyNjg3MiwianRpIjoibmJNYTFtQXh0V0xHNi1mU2RvY0twIiwidXNlcl9pZCI6IkpURkJOUCIsInNjb3BlcyI6WyJkZXZpY2VzOmxpc3QiLCJtZXNzYWdlczpyZWFkIiwibWVzc2FnZXM6d3JpdGUiLCJtZXNzYWdlczpzZW5kIiwibWVzc2FnZXM6bGlzdCJdfQ.j2r-eBtNf-BJTquS59rMGPhjHHuN2s0tm4UorJ0svYU";
+$TOKEN     = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzbXMtZ2F0ZS5hcHAiLCJzdWIiOiJKVEZCTlAiLCJleHAiOjE3ODMwMjUwNjksImlhdCI6MTc4MDQzMzA2OSwianRpIjoiMkdTQlBTa01mZ05DZnNyWkFQZHI3IiwidXNlcl9pZCI6IkpURkJOUCIsInNjb3BlcyI6WyJ0b2tlbnM6cmVmcmVzaCJdLCJvcmlnaW5hbF9zY29wZXMiOlsiZGV2aWNlczpsaXN0IiwibWVzc2FnZXM6cmVhZCIsIm1lc3NhZ2VzOndyaXRlIiwibWVzc2FnZXM6c2VuZCIsIm1lc3NhZ2VzOmxpc3QiXX0.qOgklNK2mgGvY4nEIoZ5D1VwRM5QIHz15sYo0CGvoFY";
 $DEVICE_ID = "U-ucDm6OQfO6FlCytxNIE";
 $API_BASE = "https://api.sms-gate.app/3rdparty/v1";
 
